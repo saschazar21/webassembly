53f8935996236e8c7d376439c0908fbeb92594ab
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-02-17 16:42:03 +0100

    feat(resize): added resize function

--- packages/resize/main.cpp
+++ packages/resize/main.cpp
@@ -8,12 +8,21 @@
 #define STBI_ONLY_PNG
 #include "node_modules/stb/stb_image.h"
 
+static void progress(float progress_percentage)
+{
+  printf("Resize progress: %f%%\n", progress_percentage * 100);
+}
+#define STBIR_PROGRESS_REPORT(progress_percentage) progress(progress_percentage)
+
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "node_modules/stb/stb_image_resize.h"
 
 using namespace emscripten;
 
 uint8_t *buffer;
+int width;
+int height;
+int channels;
 
 #ifdef __cplusplus
 extern "C"
@@ -38,25 +47,39 @@ enum class COLOR_CHANNELS
   RGB_ALPHA = 4
 };
 
-val load_image(std::string img, int length, COLOR_CHANNELS desired_channels = COLOR_CHANNELS::DEFAULT)
+val load(std::string img, int length, COLOR_CHANNELS desired_channels = COLOR_CHANNELS::DEFAULT)
 {
-  try
+  if (buffer != NULL)
   {
-    int width;
-    int height;
-    int channels;
-    uint8_t *img_buffer = (uint8_t *)img.c_str();
-    buffer = stbi_load_from_memory(img_buffer, length, &width, &height, &channels, (int)desired_channels);
-    printf("Image with %d x %d pixels and %d channels successfully loaded.", width, height, channels);
-    return val(typed_memory_view(sizeof(buffer) / sizeof(uint8_t), buffer));
+    free_buffer();
   }
-  catch (std::exception &e)
+  uint8_t *img_buffer = (uint8_t *)img.c_str();
+  buffer = stbi_load_from_memory(img_buffer, length, &width, &height, &channels, (int)desired_channels);
+
+  if (buffer == NULL)
   {
-    printf("Loading image buffer failed!");
-    printf("%s", e.what());
+    printf("Image loading failed!\n");
     exit(1);
   }
+
+  printf("Image with %d x %d pixels and %d channels successfully loaded.", width, height, channels);
+  return val(typed_memory_view(sizeof(buffer) / sizeof(uint8_t), buffer));
 };
+
+val resize(int output_width, int output_height)
+{
+  uint8_t stride = 0;
+  uint8_t output_stride = 0;
+  uint8_t *resized = (uint8_t *)malloc(width * height * channels);
+  if (!stbir_resize_uint8(buffer, width, height, stride, resized, output_width, output_height, output_stride, channels))
+  {
+    printf("Image resizing failed!\n");
+    exit(1);
+  }
+  free_buffer();
+  buffer = resized;
+  return val(typed_memory_view(sizeof(buffer) / sizeof(uint8_t), buffer));
+}
 
 EMSCRIPTEN_BINDINGS(resize)
 {
@@ -68,5 +91,6 @@ EMSCRIPTEN_BINDINGS(resize)
       .value("RGB_ALPHA", COLOR_CHANNELS::RGB_ALPHA);
 
   function("free_buffer", &free_buffer);
-  function("load_image", &load_image);
+  function("load", &load);
+  function("resize", &resize);
 }
